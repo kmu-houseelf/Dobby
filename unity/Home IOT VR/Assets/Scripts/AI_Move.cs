583e4b128242edc9709cdb969bb9756a1322a6e9
Kay <kthss01@naver.com>
2017-05-12 00:30:43 +0900

    유니티 에셋 일부분

---
+++ unity/Home IOT VR/Assets/Scripts/AI_Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class AI_Move : MonoBehaviour {
+
+    public GameObject vr_cam;
+    public GameObject ai; // ai character object
+    // ai pos(0,-0.9200001,0) scale(3.5,3.5,3.5)
+    private GameObject target; // target object
+    // MeMyselfEye_v1 position x,z && Main Camera rotation 
+    private AICharacterControl control;
+
+	// Use this for initialization
+	void Start () {
+        GameObject instance = Instantiate(ai) as GameObject;
+        target = GameObject.Find("WalkTarget");
+        control = instance.GetComponent<AICharacterControl>();
+        control.target = target.transform;
+        control.transform.position = new Vector3(0, -0.9200001f, 0);
+        control.transform.localScale = new Vector3(3.5f, 3.5f, 3.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        control.transform.rotation = target.transform.rotation;
+	}
+
+    void ReadTransform(Vector3 position, Quaternion rotate)
+    {
+        target.transform.position = 
+            new Vector3(position.x, target.transform.position.y, position.z);
+        target.transform.rotation = rotate;
+    }
+}
