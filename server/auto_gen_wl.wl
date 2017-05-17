e5523882d8db5445e5fb9f2cd85d295148fd5583
ngh <ngh3053@gmail.com>
2017-05-17 14:54:17 +0900

    automatic generate wl file
    protocol changed, wl file will automatically generate by config file

---
+++ server/auto_gen_wl.wl
@@ -0,0 +1,24 @@
+
+(* define morph pattern *)
+HomeiotLightLocation = Apply[Alternatives, {"거실", "마루", "방", "화장실", "부엌"}]
+HomeiotLightObject = Apply[Alternatives, {"불", "조명"}]
+HomeiotLightBrightnessFalse = Apply[Alternatives, {"어둡게"}]
+HomeiotLightBrightnessTrue = Apply[Alternatives, {"밝게"}]
+HomeiotLightActionFalse = Apply[Alternatives, {"끄", "꺼", "끌", "꺼볼", "끄기"}]
+HomeiotLightActionTrue = Apply[Alternatives, {"킬", "켜", "켜볼", "켤", "켜기", "켜줘"}]
+
+(* define sentence pattern *)
+SentencePattern1 = ___~~homeiotlightlocation:HomeiotLightLocation~~___~~homeiotlightobject:HomeiotLightObject~~___~~homeiotlightactionfalse:HomeiotLightActionFalse~~___
+SentencePattern2 = ___~~homeiotlightlocation:HomeiotLightLocation~~___~~homeiotlightobject:HomeiotLightObject~~___~~homeiotlightactiontrue:HomeiotLightActionTrue~~___
+
+(* define sentence parameter *)
+SentenceParameter1 = {homeiotlightlocation,homeiotlightobject,homeiotlightactionfalse}
+SentenceParameter2 = {homeiotlightlocation,homeiotlightobject,homeiotlightactiontrue}
+
+(* define sentence json *)
+SentenceJson1[{homeiotlightlocation_,homeiotlightobject_,homeiotlightactionfalse_}] := Module[{json = DefaultJson},json["Homeiot"]["Light"]["Location"] = homeiotlightlocation;json["Homeiot"]["Light"]["Object"] = homeiotlightobject;json["Homeiot"]["Light"]["Action"] = homeiotlightactionfalse;json["Tasktype"] = 1;json["Tts"] = homeiotlightlocation~~"의"~~homeiotlightobject~~" "~~"꺼졌습니다";json]
+SentenceJson2[{homeiotlightlocation_,homeiotlightobject_,homeiotlightactiontrue_}] := Module[{json = DefaultJson},json["Homeiot"]["Light"]["Location"] = homeiotlightlocation;json["Homeiot"]["Light"]["Object"] = homeiotlightobject;json["Homeiot"]["Light"]["Action"] = homeiotlightactiontrue;json["Tasktype"] = 1;json["Tts"] = homeiotlightlocation~~"의"~~homeiotlightobject~~" "~~"켜졌습니다";json]
+
+(* define function template *)
+FuncTemplate[SentencePattern1, SentenceParameter1, SentenceJson1]
+FuncTemplate[SentencePattern2, SentenceParameter2, SentenceJson2]
---
+++ server/genwl.py
@@ -0,0 +1,167 @@
+import codecs
+import re
+
+COMMENT_SYMBOL = '#'
+FLAG_SYMBOL = '@'
+
+LIST_FLAG = 1
+MAPPING_FLAG = 2
+PATTERN_FLAG = 3
+UNKNOWN_FALG = -1
+
+filename = 'config.txt'
+list_dict = {}
+mapping_func_dict = {}
+pattern_dict = {}
+
+def isComment(symbol):
+	return symbol == COMMENT_SYMBOL
+
+def isWhiteSpace(symbol):
+	return symbol == '\n' or symbol == '\0' or symbol == '\r'
+
+def isFlagSymbol(symbol):
+	return symbol == FLAG_SYMBOL
+
+def getFlag(line):
+	if line[1:].strip() == 'list':
+		return LIST_FLAG
+	elif line[1:].strip() == 'mapping':
+		return MAPPING_FLAG
+	elif line[1:].strip() == 'pattern':
+		return PATTERN_FLAG
+	else:
+		return UNKNOWN_FALG
+
+def run(flag, line):
+	if flag == LIST_FLAG:
+		variable, array = line.split('=')
+		variable, array	= variable.strip(), array.strip()
+		list_dict[variable] = array
+
+	elif flag == MAPPING_FLAG:
+		mapping_func, value = line.split('=')
+		mapping_func, value = mapping_func.strip(), value.strip()
+
+		mapping_func_name, parameter = re.findall(r"[\w]+", mapping_func)
+
+		if not mapping_func_name in mapping_func_dict:
+			mapping_func_dict[mapping_func_name] = {}		
+
+		mapping_func_dict[mapping_func_name][parameter] = value
+
+	elif flag == PATTERN_FLAG:
+		pattern, json_rule = line.split('/')
+		pattern, json_rule = pattern.strip(), json_rule.strip()
+		pattern_dict[pattern] = json_rule
+
+with codecs.open(filename,'r',encoding='utf8') as f:
+    for line in f.readlines():
+    	text = line.strip('\n')
+    	symbol = text[0]
+
+    	if isComment(symbol) or isWhiteSpace(symbol):
+    		continue
+    	elif isFlagSymbol(symbol):
+    		flag = getFlag(text)
+    	else:
+    		run(flag, text)
+
+
+def printResult():
+	print('# list table')
+	for variable, array in list_dict.items():
+	    print(variable, array)
+
+	print('# mapping table')
+	for func_name, pair in mapping_func_dict.items():
+		print(func_name)
+		for parameter, value in pair.items():
+			print(parameter, value)
+
+	print('# pattern table')
+	for pattern, json_rule in pattern_dict.items():
+		print(pattern, json_rule)
+
+def generateWl():
+	with codecs.open('auto_gen_wl.wl','w',encoding='utf8') as f:
+		f.write('\n(* define morph pattern *)\n')
+		for variable, array in list_dict.items():
+			f.write(variable + ' = Apply[Alternatives, ' + array + ']\n')
+
+		f.write('\n(* define sentence pattern *)\n')
+		pattern_number = 1
+		for pattern, _ in pattern_dict.items():
+			lst = re.findall(r"[\w]+", pattern)
+			lst = ['{}:{}'.format(item.lower(), item) for item in lst]
+			pat = '~~___~~'.join(lst)
+			pat = '___~~' + pat + '~~___'
+			f.write('SentencePattern{} = {}\n'.format(pattern_number, pat))
+			pattern_number += 1
+
+		f.write('\n(* define sentence parameter *)\n')
+		parameter_number = 1
+		for pattern, _ in pattern_dict.items():
+			lst = re.findall(r"[\w]+", pattern)
+			lst = [item.lower() for item in lst]
+			arr = ','.join(lst)
+			f.write('SentenceParameter{} = {{{}}}\n'.format(parameter_number, arr))
+			parameter_number += 1
+
+		f.write('\n(* define sentence json *)\n')
+		json_number = 1
+		for pattern, json_rule in pattern_dict.items():
+			# paramter 
+			lst = re.findall(r"[\w]+", pattern)
+			lst2 = ['{}_'.format(item.lower()) for item in lst]
+			arr = ','.join(lst2)
+			json = ''
+
+			# paramter json 
+			for item in lst:
+				temp = re.findall(r"[A-Z][a-z]+", item)
+				temp = ['[\"{}\"]'.format(t) for t in temp if t != "True" and t != 'False' ]
+				lhs = 'json'+''.join(temp)
+				rhs = item.lower() 
+				json += '{} = {};'.format(lhs, rhs)
+
+			# rule json
+			rules = json_rule.split(',')
+			for rule in rules:
+				lhs, rhs = rule.split('=')
+				lhs, rhs = lhs.strip(), rhs.strip()
+				temp = re.findall(r"[A-Z][a-z]+", lhs)
+				temp = ['[\"{}\"]'.format(t) for t in temp if t != "True" and t != 'False' ]
+				lhs = 'json'+''.join(temp)
+
+				rhs = [item.strip() for item in rhs.split('+')]
+
+				for i, item in enumerate(rhs):
+					if re.match(r'[\w]+\[[\w]+\]', item):
+						func, param = re.findall(r"[\w]+", item)
+						if func in mapping_func_dict and param in mapping_func_dict[func]:
+							rhs[i] = mapping_func_dict[func][param]
+
+				rhs = [item.lower() if item in list_dict else item for item in rhs]
+				rhs = '~~'.join(rhs)
+
+				json += lhs + ' = ' + rhs + ';'
+
+			f.write('SentenceJson{}[{{{}}}] := Module[{{json = DefaultJson}},'.format(json_number, arr) + json + 'json]\n')
+			json_number += 1
+
+		f.write('\n(* define function template *)\n')
+		for i in range(json_number - 1):
+			f.write('FuncTemplate[SentencePattern{0}, SentenceParameter{0}, SentenceJson{0}]\n'.format(i + 1))
+		
+
+printResult()
+generateWl()
+
+
+
+
+
+# process Unicode text
+#with codecs.open(filename,'w',encoding='utf8') as f:
+#    f.write(text)
